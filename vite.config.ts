42cf932c6497afbbedf4309b51ddf49ecb24cf31
Samuel Hermany <samuelhermany@hotmail.com>
2025-04-16 18:20:24 -0300

    build: Alterações de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dxf-parametric-cut',
   server: {
     open: true, // Isso abre o navegador automaticamente
     port: 3000, // Define a porta que você quer usar
